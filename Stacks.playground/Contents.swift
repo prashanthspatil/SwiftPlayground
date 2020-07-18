527ef4414b4cafe7ad6d384daadc78d205219c3b
Prashanth <Prashanth@shoptreeapp.com>
2020-07-18 21:31:31 +0530

    Implementation verify if string has balanced paranthesis

--- Stacks.playground/Contents.swift
+++ Stacks.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// Given a string, find if it’s open and close parentheses are balanced.
+
 struct Stack<T> {
   private var array: [T] = []
 
@@ -19,3 +21,56 @@ struct Stack<T> {
     return array.isEmpty
   }
 }
+
+extension Character {
+  func isMatchingPair(closing: Character) -> Bool {
+    switch self {
+    case "(":
+      return closing == ")"
+    case "[":
+      return closing == "]"
+    case "{":
+      return closing == "}"
+    default:
+      return false
+    }
+  }
+}
+
+extension String {
+  var openingParentheses: [String] {
+    return ["(", "[", "{"]
+  }
+
+  var closingParentheses: [String] {
+    return [")", "}", "]"]
+  }
+
+  var hasBalancedParentheses: Bool {
+    var parentheses: Stack<Character> = Stack()
+
+    for char in self {
+
+      if parentheses.isEmpty {
+        if openingParentheses.contains("\(char)") {
+          parentheses.push(element: char)
+        } else if closingParentheses.contains("\(char)") {
+          return false
+        }
+      } else {
+        if let top = parentheses.peek() {
+          if top.isMatchingPair(closing: char) {
+            parentheses.pop()
+          } else if openingParentheses.contains("\(char)") {
+            parentheses.push(element: char)
+          }
+        }
+      }
+    }
+    return parentheses.isEmpty
+  }
+}
+
+var teststring="({})"
+
+teststring.hasBalancedParentheses
